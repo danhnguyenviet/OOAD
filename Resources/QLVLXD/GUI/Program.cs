a78a6c39463eb9149ad72c6a69a4b853902243bb
truonghtn <truonghtn@gmail.com>
2015-12-27 12:22:40 +0700

    Tách nhánh

--- Resources/QLVLXD/GUI/Program.cs
+++ Resources/QLVLXD/GUI/Program.cs
@@ -15,7 +15,7 @@ namespace GUI_QLVLXD
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new FrmMain());
         }
     }
 }
