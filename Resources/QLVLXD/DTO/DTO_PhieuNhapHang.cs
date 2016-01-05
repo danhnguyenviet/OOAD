aa9f16921078df2350e3e7df13d7337e82db93fc
truonghtn <truonghtn@gmail.com>
2016-01-06 00:03:39 +0700

    Push lại

--- Resources/QLVLXD/DTO/DTO_PhieuNhapHang.cs
+++ Resources/QLVLXD/DTO/DTO_PhieuNhapHang.cs
@@ -5,7 +5,64 @@ using System.Text;
 
 namespace DTO
 {
-    class DTO_PhieuNhapHang
+    public class DTO_PhieuNhapHang
     {
+        private string _maPN;
+        private string _maNV;
+        private string _maNCC;
+        private DateTime _thoiGianLap;
+
+        public string MaPN
+        {
+            get
+            {
+                return _maPN;
+            }
+
+            set
+            {
+                _maPN = value;
+            }
+        }
+
+        public string MaNV
+        {
+            get
+            {
+                return _maNV;
+            }
+
+            set
+            {
+                _maNV = value;
+            }
+        }
+
+        public DateTime ThoiGianLap
+        {
+            get
+            {
+                return _thoiGianLap;
+            }
+
+            set
+            {
+                _thoiGianLap = value;
+            }
+        }
+
+        public string MaNCC
+        {
+            get
+            {
+                return _maNCC;
+            }
+
+            set
+            {
+                _maNCC = value;
+            }
+        }
     }
+
 }
